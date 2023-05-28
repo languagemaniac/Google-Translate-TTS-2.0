dbab1160b130370d17285ae1424c68a11ed5f80d
languagemaniac <43100450+languagemaniac@users.noreply.github.com>
2023-05-28 05:08:15 +0200

    Update GoogleTTS.py
    1. Made it work with python 3
    2. Changed the api link as I got 404 errors, I guess the api used in the original code doesn't exist anymore
    3. Made it support UTF-8, so it's compatible with japanese and chinese and many other languages.
    4. Implemented a welcome screen so instead of having to write a command with arguments you just have to answer 3 questions: Language, speed, and filename.
    5. Added a way for it to download multiple chunks of text simultaneously so it downloads faster, and added a progress bar too

--- GoogleTTS.py
+++ GoogleTTS.py
@@ -1,143 +1,113 @@
-#!/usr/bin/python
-
 import sys
 import argparse
 import re
-import urllib, urllib2
+import urllib.parse
+import urllib.request
 import time
 from collections import namedtuple
-
+import os
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
 def split_text(input_text, max_length=100):
-    """
-    Try to split between sentences to avoid interruptions mid-sentence.
-    Failing that, split between words.
-    See split_text_rec
-    """
+    
     def split_text_rec(input_text, regexps, max_length=max_length):
-        """
-        Split a string into substrings which are at most max_length.
-        Tries to make each substring as big as possible without exceeding
-        max_length.
-        Will use the first regexp in regexps to split the input into
-        substrings.
-        If it it impossible to make all the segments less or equal than
-        max_length with a regexp then the next regexp in regexps will be used
-        to split those into subsegments.
-        If there are still substrings who are too big after all regexps have
-        been used then the substrings, those will be split at max_length.
-
-        Args:
-            input_text: The text to split.
-            regexps: A list of regexps.
-                If you want the separator to be included in the substrings you
-                can add parenthesis around the regular expression to create a
-                group. Eg.: '[ab]' -> '([ab])'
-
-        Returns:
-            a list of strings of maximum max_length length.
-        """
-        if(len(input_text) <= max_length): return [input_text]
-
-        #mistakenly passed a string instead of a list
-        if isinstance(regexps, basestring): regexps = [regexps]
+        
+        if len(input_text) <= max_length:
+            return [input_text]
+        
+        if isinstance(regexps, str):
+            regexps = [regexps]
         regexp = regexps.pop(0) if regexps else '(.{%d})' % max_length
-
+        
         text_list = re.split(regexp, input_text)
         combined_text = []
-        #first segment could be >max_length
         combined_text.extend(split_text_rec(text_list.pop(0), regexps, max_length))
         for val in text_list:
             current = combined_text.pop()
             concat = current + val
-            if(len(concat) <= max_length):
+            if len(concat) <= max_length:
                 combined_text.append(concat)
             else:
                 combined_text.append(current)
-                #val could be >max_length
                 combined_text.extend(split_text_rec(val, regexps, max_length))
         return combined_text
 
-    return split_text_rec(input_text.replace('\n', ''),
-                          ['([\,|\.|;]+)', '( )'])
+    return split_text_rec(input_text.replace('\n', ''), ['([\,|\.|;]+)', '( )'])
 
+audio_args = namedtuple('audio_args', ['output'])
 
-audio_args = namedtuple('audio_args',['language','output'])
+LANGUAGE_TAGS = {
+    'English': 'en-us',
+    'French': 'fr-fr',
+    'Mandarin chinese': 'zh-cn',
+    'Japanese': 'ja-jp'
+}
 
-def audio_extract(input_text='',args=None):
-    # This accepts :
-    #   a dict,
-    #   an audio_args named tuple
-    #   or arg parse object
-    if args is None:
-        args = audio_args(language='en',output=open('output.mp3', 'w'))
-    if type(args) is dict:
-        args = audio_args(
-                    language=args.get('language','en'),
-                    output=open(args.get('output','output.mp3'), 'w')
-        )
-    #process input_text into chunks
-    #Google TTS only accepts up to (and including) 100 characters long texts.
-    #Split the text in segments of maximum 100 characters long.
+def get_language_tag(language):
+    return LANGUAGE_TAGS.get(language.lower())
+
+def download_chunk(mp3url, headers):
+    try:
+        response = urllib.request.urlopen(mp3url)
+        return response.read()
+    except urllib.error.URLError as e:
+        print('%s' % e)
+        return None
+
+def audio_extract(input_text='', language='en-us', speed=0.4, output_filename='output.mp3'):
+    args = audio_args(output=open(output_filename, 'wb'))
     combined_text = split_text(input_text)
 
-    #download chunks and write them to the output file
-    for idx, val in enumerate(combined_text):
-        mp3url = "http://translate.google.com/translate_tts?tl=%s&q=%s&total=%s&idx=%s" % (
-            args.language,
-            urllib.quote(val),
-            len(combined_text),
-            idx)
-        headers = {"Host": "translate.google.com",
-                   "Referer": "http://www.gstatic.com/translate/sound_player2.swf",
-                   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) "
-                                 "AppleWebKit/535.19 (KHTML, like Gecko) "
-                                 "Chrome/18.0.1025.163 Safari/535.19"
-        }
-        req = urllib2.Request(mp3url, '', headers)
-        sys.stdout.write('.')
-        sys.stdout.flush()
-        if len(val) > 0:
-            try:
-                response = urllib2.urlopen(req)
-                args.output.write(response.read())
-                time.sleep(.5)
-            except urllib2.URLError as e:
-                print ('%s' % e)
-    args.output.close()
-    print('Saved MP3 to %s' % args.output.name)
+    progress_bar = tqdm(total=len(combined_text), ncols=80, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}')
 
+    max_workers = 20
+    downloaded_chunks = {}
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for idx, val in enumerate(combined_text):
+            mp3url = f"https://www.google.com/speech-api/v1/synthesize?text={urllib.parse.quote(val)}&enc=mpeg&lang={language}&speed={speed}&client=lr-language-tts&use_google_only_voices=1"
+            headers = {
+                "Host": "www.google.com",
+                "Referer": "http://www.gstatic.com/translate/sound_player2.swf",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.163 Safari/535.19"
+            }
+            future = executor.submit(download_chunk, mp3url, headers)
+            futures.append((future, idx))
+
+        for future, idx in futures:
+            data = future.result()
+            downloaded_chunks[idx] = data
+            progress_bar.update(1)
+
+    for idx in range(len(combined_text)):
+        if idx in downloaded_chunks and downloaded_chunks[idx]:
+            args.output.write(downloaded_chunks[idx])
+
+    args.output.close()
+    print(f"Saved MP3 to {args.output.name}")
 
 def text_to_speech_mp3_argparse():
-    description = 'Google TTS Downloader.'
-    parser = argparse.ArgumentParser(description=description,
-                                     epilog='tunnel snakes rule')
-    parser.add_argument('-o', '--output',
-                        action='store', nargs='?',
-                        help='Filename to output audio to',
-                        type=argparse.FileType('wb'), default='out.mp3')
-    parser.add_argument('-l', '--language',
-                        action='store',
-                        nargs='?',
-                        help='Language to output text to.', default='en')
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--file',
-                       type=argparse.FileType('r'),
-                       help='File to read text from.')
-    group.add_argument('-s', '--string',
-                       action='store',
-                       nargs='+',
-                       help='A string of text to convert to speech.')
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
-    return parser.parse_args()
-    
+    print("Welcome to Google TTS audiobook maker")
+    print("Please select your document's language:")
+    for idx, language in enumerate(LANGUAGE_TAGS.keys(), 1):
+        print(f"{idx}. {language}")
+    language_selection = int(input("Select language: "))
+    language_list = list(LANGUAGE_TAGS.keys())
+    selected_language = language_list[language_selection - 1]
+    language_tag = get_language_tag(selected_language)
+
+    speed = float(input("Please select the speed at which you want your file to be read: "))
+    file_path = input("Input file name: ")
+
+    base_name = os.path.splitext(file_path)[0]
+    output_filename = base_name + ".mp3"
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        input_text = file.read()
+
+    audio_extract(input_text=input_text, language=language_tag, speed=speed, output_filename=output_filename)
 
 if __name__ == "__main__":
-    args = text_to_speech_mp3_argparse()
-    if args.file:
-        input_text = args.file.read()
-    if args.string:
-        input_text = ' '.join(map(str, args.string))
-    audio_extract(input_text=input_text, args=args)
+    text_to_speech_mp3_argparse()
